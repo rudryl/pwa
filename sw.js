dd3039549a2ae3dcd95dde0649fc49ceff53fb06
Rudryl <rudrylontsi@gmail.com>
2025-03-04 00:01:02 -0800

    initial commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+// sw.js
+const CACHE_NAME = 'my-mobile-pwa-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
